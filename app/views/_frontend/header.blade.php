81a055e72904bd45ea99f5b5c1ace03fde7a3f34
Gabor Molnar <gabormolnar92@gmail.com>
2015-03-31 21:17:29 +0200

    Header update

--- app/views/_frontend/header.blade.php
+++ app/views/_frontend/header.blade.php
@@ -1,16 +1,16 @@
 <header>
     <div class="container">
         <div class="header-pictures">
-            <div class="grid-sizer"></div>
             <div class="picture-pokrova header-element"></div>
+            <div class="picture-templom-1 header-element"></div>
             <h1 class="picture-cross header-element">Görög Katolikus kereszt</h1>
 
-            <div class="picture-templom-1 header-element"></div>
+
             <div class="picture-templom-2 header-element"></div>
             <div class="picture-templom-3 header-element"></div>
-            <h1 class="header-element site-title">{{Setting::get('site-title')}}</h1>
 
             <div class="picture-templom-4 header-element"></div>
+            <h1 class="header-element site-title">{{Setting::get('site-title')}}</h1>
         </div>
     </div>
 </header>
