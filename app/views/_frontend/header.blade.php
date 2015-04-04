cfd0798a19e1df8071ff8a1a1e9f0969953c87d3
Gabor Molnar <gabormolnar92@gmail.com>
2015-04-04 16:12:08 +0200

    Webradio

--- app/views/_frontend/header.blade.php
+++ app/views/_frontend/header.blade.php
@@ -22,3 +22,30 @@
         </ul>
     </div>
 </nav>
+
+<section class="webradio">
+    <div class="container">
+        <h1 class="player-title">Webrádió</h1>
+
+        <div class="player-wrapper">
+            <audio class="audio-player" preload="auto" controls>
+                <source src="http://online.szentistvanradio.hu:7000/;&type=mp3" type="audio/mpeg"/>
+            </audio>
+        </div>
+
+        <form class="form-inline">
+            <div class="form-group">
+                <label>Válasszon egy rádióállomást</label>
+                <select name="radio" class="form-control input-sm radio-select">
+                    <option value="http://92.43.201.24:8000/mr">Mária rádió</option>
+                    <option selected value="http://online.szentistvanradio.hu:7000/;&type=mp3">Szent István
+                        rádió
+                    </option>
+                    <option value="http://katolikusradio.hu:9000/live_low.mp3">Katolikus rádió
+                    </option>
+                </select>
+            </div>
+        </form>
+
+    </div>
+</section>
